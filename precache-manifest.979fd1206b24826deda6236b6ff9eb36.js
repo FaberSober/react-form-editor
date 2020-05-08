d45213d79c5059854aba6d5c191bea8c55eed87e
xupengfei <faberxu@gmail.com>
2020-05-08 16:25:27 +0800

    Updates

---
+++ precache-manifest.979fd1206b24826deda6236b6ff9eb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e0facb7dadefb4d5f85deda2aa7f18",
+    "url": "/react-form-editor/index.html"
+  },
+  {
+    "revision": "4370fbaaab0e81e89c50",
+    "url": "/react-form-editor/static/css/2.361cdfab.chunk.css"
+  },
+  {
+    "revision": "683ee609d6e9e3af2eb9",
+    "url": "/react-form-editor/static/css/main.200c9609.chunk.css"
+  },
+  {
+    "revision": "4370fbaaab0e81e89c50",
+    "url": "/react-form-editor/static/js/2.c0683469.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-form-editor/static/js/2.c0683469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683ee609d6e9e3af2eb9",
+    "url": "/react-form-editor/static/js/main.b8646004.chunk.js"
+  },
+  {
+    "revision": "b3bc75acf162dcf50531",
+    "url": "/react-form-editor/static/js/runtime-main.c21d2aa2.js"
+  }
+]);
